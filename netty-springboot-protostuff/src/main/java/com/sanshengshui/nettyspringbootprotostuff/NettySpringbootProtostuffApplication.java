a2d1f6247d1618f513efb72cf82e572108bd5dbc
sanshengshui <lovewsic@gmail.com>
2018-09-19 11:22:21 +0800

    netty整合springboot并整合protostuff进行数据传输

---
+++ netty-springboot-protostuff/src/main/java/com/sanshengshui/nettyspringbootprotostuff/NettySpringbootProtostuffApplication.java
@@ -0,0 +1,12 @@
+package com.sanshengshui.nettyspringbootprotostuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettySpringbootProtostuffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettySpringbootProtostuffApplication.class, args);
+    }
+}
