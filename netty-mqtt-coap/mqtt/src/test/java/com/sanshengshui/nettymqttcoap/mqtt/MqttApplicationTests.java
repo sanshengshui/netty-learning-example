df49b71d6a2b98f98b6b339bfa4efe13347c1a2c
sanshengshui <lovewsic@gmail.com>
2018-09-29 00:42:36 +0800

    :building_construction: 更改项目目录和结构

---
+++ netty-mqtt-coap/mqtt/src/test/java/com/sanshengshui/nettymqttcoap/mqtt/MqttApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sanshengshui.nettymqttcoap.mqtt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MqttApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
