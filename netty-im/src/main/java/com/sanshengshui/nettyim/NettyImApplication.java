1c03540edc88ff7561bf73e063d14643da72c713
sanshengshui <lovewsic@gmail.com>
2018-10-04 16:22:55 +0800

    :rocket: 添加netty之用netty实现IM功能(物联网,推送皆可！)

---
+++ netty-im/src/main/java/com/sanshengshui/nettyim/NettyImApplication.java
@@ -0,0 +1,12 @@
+package com.sanshengshui.nettyim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyImApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyImApplication.class, args);
+    }
+}
---
+++ netty-im/src/test/java/com/sanshengshui/nettyim/NettyImApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sanshengshui.nettyim;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NettyImApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
