08e150a5f66949111fe64cf02b0d9d1ef8057459
sanshengshui <lovewsic@gmail.com>
2018-10-04 16:14:29 +0800

    :rocket: 添加以netty为通讯框架实现RPC

---
+++ netty-rpc/src/main/java/com/sanshengshui/rpc/NettyRpcApplication.java
@@ -0,0 +1,12 @@
+package com.sanshengshui.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyRpcApplication.class, args);
+    }
+}
---
+++ netty-rpc/src/test/java/com/sanshengshui/rpc/NettyRpcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sanshengshui.rpc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NettyRpcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
