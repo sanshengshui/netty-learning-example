d0a0deecf657f34a0a569704b590bbe0e60bfa98
sanshengshui <lovewsic@gmail.com>
2018-10-17 20:02:47 +0800

    :rocket: mqtt断开处理

--- netty-mqtt-coap/mqtt/src/main/java/com/sanshengshui/mqtt/protocol/DisConnect.java
+++ netty-mqtt-coap/mqtt/src/main/java/com/sanshengshui/mqtt/protocol/DisConnect.java
@@ -2,6 +2,7 @@ package com.sanshengshui.mqtt.protocol;
 
 import io.netty.channel.Channel;
 import io.netty.handler.codec.mqtt.MqttMessage;
+import io.netty.util.AttributeKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +14,12 @@ public class DisConnect {
     private static final Logger LOGGER = LoggerFactory.getLogger(DisConnect.class);
 
     public void processDisConnect(Channel channel, MqttMessage msg){
-
+        String clientId = (String) channel.attr(AttributeKey.valueOf("clientId")).get();
+        /**
+         * TODO
+         * 服务端在收到DISCONNECT报文时：
+         * 必须丢弃任何与当前连接关联的未发布的遗嘱消息
+         */
+        channel.close();
     }
 }
