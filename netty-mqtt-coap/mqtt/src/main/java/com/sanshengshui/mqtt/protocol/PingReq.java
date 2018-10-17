524da1edc1309389645d8a2e1f9ba3a770ee9687
sanshengshui <lovewsic@gmail.com>
2018-10-17 20:09:56 +0800

    :rocket: mqtt心跳请求处理

--- netty-mqtt-coap/mqtt/src/main/java/com/sanshengshui/mqtt/protocol/PingReq.java
+++ netty-mqtt-coap/mqtt/src/main/java/com/sanshengshui/mqtt/protocol/PingReq.java
@@ -1,7 +1,8 @@
 package com.sanshengshui.mqtt.protocol;
 
 import io.netty.channel.Channel;
-import io.netty.handler.codec.mqtt.MqttMessage;
+import io.netty.handler.codec.mqtt.*;
+import io.netty.util.AttributeKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +14,13 @@ public class PingReq {
     private static final Logger LOGGER = LoggerFactory.getLogger(PingReq.class);
 
     public void processPingReq(Channel channel, MqttMessage msg){
+        MqttMessage pingPespMessage = MqttMessageFactory.newMessage(
+                new MqttFixedHeader(MqttMessageType.PINGRESP,false, MqttQoS.AT_MOST_ONCE,false,0),
+                null,
+                null
+        );
+        LOGGER.debug("PINGREQ - clientId: {}", (String) channel.attr(AttributeKey.valueOf("clientId")).get());
+        channel.writeAndFlush(pingPespMessage);
 
     }
 }
