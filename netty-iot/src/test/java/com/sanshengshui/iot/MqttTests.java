87a0af6df15ec2e2a2b7fdcd573ff579a9f032d0
sanshengshui <lovewsic@gmail.com>
2018-10-20 23:21:26 +0800

    :white_check_mark: 添加测试

--- netty-iot/src/test/java/com/sanshengshui/iot/MqttTests.java
+++ netty-iot/src/test/java/com/sanshengshui/iot/MqttTests.java
@@ -1,5 +1,6 @@
 package com.sanshengshui.iot;
 
+import com.sanshengshui.iot.common.message.DupPublishMessageStore;
 import com.sanshengshui.iot.common.session.SessionStore;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -10,6 +11,8 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.Serializable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,8 +21,12 @@ public class MqttTests {
     @Autowired
     private RedisTemplate<String, Serializable> redisCacheTemplate;
 
-    public static final String CACHE_PRE = "groza:";
+    public static final String CACHE_SES = "groza:session:";
+    public static final String CACHE_DUPPub = "groza:duppub";
 
+    /**
+     * 会话存储测试
+     */
     @Test
     public void GrozaSessionStoreTest(){
 
@@ -27,10 +34,34 @@ public class MqttTests {
         sessionStore.setChannelId("20181018163127");
         sessionStore.setCleanSession(true);
         sessionStore.setClientId("111");
-        redisCacheTemplate.opsForValue().set("graza:20181018163127",sessionStore);
-        final SessionStore sessionStore1 = (SessionStore) redisCacheTemplate.opsForValue().get("graza:20181018163127");
-        log.info("[对象缓存结果] - [{}]", sessionStore1);
-        log.info(sessionStore.toString());
+        redisCacheTemplate.opsForValue().set(CACHE_SES + "20181018163127",sessionStore);
+        final SessionStore sessionStore1 = (SessionStore) redisCacheTemplate.opsForValue().get(CACHE_SES + "20181018163127");
+        log.info("[对象缓存结果] - [{}]", sessionStore1.toString());
+
+    }
+
+    @Test
+    public void GrozaDupPublishMessageStoreTest(){
+        DupPublishMessageStore dupPublishMessageStore = new DupPublishMessageStore();
+        dupPublishMessageStore.setClientId("20181020112852");
+        dupPublishMessageStore.setMessageId(123456);
+        dupPublishMessageStore.setMqttQoS(1);
+        dupPublishMessageStore.setTopic("test");
+        dupPublishMessageStore.setMessageBytes(new byte[]{1,2,3,4,5,6});
+        redisCacheTemplate.opsForHash().put(CACHE_DUPPub + "20181020112852",123456,dupPublishMessageStore);
+        redisCacheTemplate.opsForHash().put(CACHE_DUPPub + "20181020112852",123457,dupPublishMessageStore);
+        redisCacheTemplate.hasKey(CACHE_DUPPub + "20181020112852");
+        log.info(redisCacheTemplate.hasKey(CACHE_DUPPub + "20181020112852").toString());
+        Map<Object,Object> map1 = redisCacheTemplate.opsForHash().entries(CACHE_DUPPub + "20181020112852");
+        if (map1 != null && !map1.isEmpty()) {
+            map1.forEach((k, v) -> {
+                log.info(k.toString());
+                 DupPublishMessageStore dupPublishMessageStore1 = (DupPublishMessageStore) v;
+                 log.info(dupPublishMessageStore1.getTopic());
+            });
+        }
+        redisCacheTemplate.opsForHash().delete(CACHE_DUPPub + "20181020112852",123456);
+        redisCacheTemplate.delete(CACHE_DUPPub + "20181020112852");
 
     }
 }
