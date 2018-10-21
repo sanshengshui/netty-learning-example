44fadec656a901a177803b055e7c0354082b859e
sanshengshui <lovewsic@gmail.com>
2018-10-21 09:00:50 +0800

    :beers: 断开连接DISCONNECT处理

--- netty-iot/src/main/java/com/sanshengshui/iot/protocol/DisConnect.java
+++ netty-iot/src/main/java/com/sanshengshui/iot/protocol/DisConnect.java
@@ -1,6 +1,10 @@
 package com.sanshengshui.iot.protocol;
 
+import com.sanshengshui.iot.common.message.GrozaDupPubRelMessageStoreService;
+import com.sanshengshui.iot.common.message.GrozaDupPublishMessageStoreService;
+import com.sanshengshui.iot.common.session.GrozaSessionStoreService;
 import com.sanshengshui.iot.common.session.SessionStore;
+import com.sanshengshui.iot.common.subscribe.GrozaSubscribeStoreService;
 import io.netty.channel.Channel;
 import io.netty.handler.codec.mqtt.MqttMessage;
 import io.netty.util.AttributeKey;
@@ -14,5 +18,35 @@ import org.slf4j.LoggerFactory;
 public class DisConnect {
     private static final Logger LOGGER = LoggerFactory.getLogger(DisConnect.class);
 
+    private GrozaSessionStoreService grozaSessionStoreService;
+
+    private GrozaSubscribeStoreService grozaSubscribeStoreService;
+
+    private GrozaDupPublishMessageStoreService grozaDupPublishMessageStoreService;
+
+    private GrozaDupPubRelMessageStoreService grozaDupPubRelMessageStoreService;
+
+    public DisConnect(GrozaSessionStoreService grozaSessionStoreService,
+                      GrozaSubscribeStoreService grozaSubscribeStoreService,
+                      GrozaDupPublishMessageStoreService grozaDupPublishMessageStoreService,
+                      GrozaDupPubRelMessageStoreService grozaDupPubRelMessageStoreService){
+        this.grozaSessionStoreService = grozaSessionStoreService;
+        this.grozaSubscribeStoreService = grozaSubscribeStoreService;
+        this.grozaDupPublishMessageStoreService = grozaDupPublishMessageStoreService;
+        this.grozaDupPubRelMessageStoreService = grozaDupPubRelMessageStoreService;
+    }
+
+    public void processDisConnect(Channel channel,MqttMessage msg){
+        String clientId = (String) channel.attr(AttributeKey.valueOf("clientId")).get();
+        SessionStore sessionStore = grozaSessionStoreService.get(clientId);
+        if (sessionStore!=null && sessionStore.isCleanSession()){
+            grozaSubscribeStoreService.removeForClient(clientId);
+            grozaDupPublishMessageStoreService.removeByClient(clientId);
+            grozaDupPubRelMessageStoreService.removeByClient(clientId);
+        }
+        LOGGER.debug("DISCONNECT - clientId: {}, cleanSession: {}", clientId, sessionStore.isCleanSession());
+        grozaSessionStoreService.remove(clientId);
+        channel.close();
+    }
 
 }
