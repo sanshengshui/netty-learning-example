dcea152d80b9553e1a813d22505bef93cff7a7e0
sanshengshui <lovewsic@gmail.com>
2018-10-21 09:16:16 +0800

    :beers: PUBCOMP连接处理

--- netty-iot/src/main/java/com/sanshengshui/iot/protocol/PubAck.java
+++ netty-iot/src/main/java/com/sanshengshui/iot/protocol/PubAck.java
@@ -1,4 +1,30 @@
 package com.sanshengshui.iot.protocol;
 
+import com.sanshengshui.iot.common.message.GrozaDupPublishMessageStoreService;
+import io.netty.channel.Channel;
+import io.netty.handler.codec.mqtt.MqttMessageIdVariableHeader;
+import io.netty.util.AttributeKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author james
+ * @date 2018年10月21日 09:02
+ * PUBACK连接处理
+ */
 public class PubAck {
+    public static final Logger LOGGER = LoggerFactory.getLogger(PubAck.class);
+
+    private GrozaDupPublishMessageStoreService grozaDupPublishMessageStoreService;
+
+    public PubAck(GrozaDupPublishMessageStoreService grozaDupPublishMessageStoreService){
+        this.grozaDupPublishMessageStoreService = grozaDupPublishMessageStoreService;
+    }
+
+    public void processPubAck(Channel channel, MqttMessageIdVariableHeader variableHeader){
+        int messageId = variableHeader.messageId();
+        LOGGER.debug("PUBACK - clientId: {}, messageId: {}", (String) channel.attr(AttributeKey.valueOf("clientId")).get(), messageId);
+        grozaDupPublishMessageStoreService.remove((String) channel.attr(AttributeKey.valueOf("clientId")).get(), messageId);
+
+    }
 }
--- netty-iot/src/main/java/com/sanshengshui/iot/protocol/PubComp.java
+++ netty-iot/src/main/java/com/sanshengshui/iot/protocol/PubComp.java
@@ -1,4 +1,29 @@
 package com.sanshengshui.iot.protocol;
 
+import com.sanshengshui.iot.common.message.GrozaDupPubRelMessageStoreService;
+import io.netty.channel.Channel;
+import io.netty.handler.codec.mqtt.MqttMessageIdVariableHeader;
+import io.netty.util.AttributeKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author james
+ * @date 2018年10月21日09:08
+ * PUBCOMP连接处理
+ */
 public class PubComp {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PubComp.class);
+
+    private GrozaDupPubRelMessageStoreService grozaDupPubRelMessageStoreService;
+
+    public PubComp(GrozaDupPubRelMessageStoreService grozaDupPubRelMessageStoreService){
+        this.grozaDupPubRelMessageStoreService = grozaDupPubRelMessageStoreService;
+    }
+
+    public void processPubComp(Channel channel, MqttMessageIdVariableHeader variableHeader){
+        int messageId = variableHeader.messageId();
+        LOGGER.debug("PUBCOMP - clientId: {}, messageId: {}", (String) channel.attr(AttributeKey.valueOf("clientId")).get(), messageId);
+        grozaDupPubRelMessageStoreService.remove((String)channel.attr(AttributeKey.valueOf("clientId")).get(), variableHeader.messageId());
+    }
 }
