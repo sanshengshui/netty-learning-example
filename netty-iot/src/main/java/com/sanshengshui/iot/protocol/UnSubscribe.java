e3357fc6eb20de78f10de16ae189409187e6bd2d
sanshengshui <lovewsic@gmail.com>
2018-10-21 09:36:30 +0800

    :beers: 取消订阅

--- netty-iot/src/main/java/com/sanshengshui/iot/protocol/UnSubscribe.java
+++ netty-iot/src/main/java/com/sanshengshui/iot/protocol/UnSubscribe.java
@@ -1,4 +1,33 @@
 package com.sanshengshui.iot.protocol;
 
+import com.sanshengshui.iot.common.subscribe.GrozaSubscribeStoreService;
+import io.netty.channel.Channel;
+import io.netty.handler.codec.mqtt.*;
+import io.netty.util.AttributeKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
 public class UnSubscribe {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UnSubscribe.class);
+
+    private GrozaSubscribeStoreService grozaSubscribeStoreService;
+
+    public UnSubscribe(GrozaSubscribeStoreService grozaSubscribeStoreService){
+        this.grozaSubscribeStoreService = grozaSubscribeStoreService;
+    }
+
+    public void processUnSubscribe(Channel channel, MqttUnsubscribeMessage msg) {
+        List<String> topicFilters = msg.payload().topics();
+        String clinetId = (String) channel.attr(AttributeKey.valueOf("clientId")).get();
+        topicFilters.forEach(topicFilter -> {
+            grozaSubscribeStoreService.remove(topicFilter, clinetId);
+            LOGGER.debug("UNSUBSCRIBE - clientId: {}, topicFilter: {}", clinetId, topicFilter);
+        });
+        MqttUnsubAckMessage unsubAckMessage = (MqttUnsubAckMessage) MqttMessageFactory.newMessage(
+                new MqttFixedHeader(MqttMessageType.UNSUBACK, false, MqttQoS.AT_MOST_ONCE, false, 0),
+                MqttMessageIdVariableHeader.from(msg.variableHeader().messageId()), null);
+        channel.writeAndFlush(unsubAckMessage);
+    }
 }
