245bae8b2fa19599827a97ed9aa97353aa838d6d
sanshengshui <lovewsic@gmail.com>
2018-10-21 09:34:37 +0800

    :beers: 发布释放

--- netty-iot/src/main/java/com/sanshengshui/iot/protocol/PubRel.java
+++ netty-iot/src/main/java/com/sanshengshui/iot/protocol/PubRel.java
@@ -1,4 +1,25 @@
 package com.sanshengshui.iot.protocol;
 
+import io.netty.channel.Channel;
+import io.netty.handler.codec.mqtt.*;
+import io.netty.util.AttributeKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * PUBREL连接处理
+ * @author james
+ * @date 2018年10月21日 09:26
+ */
 public class PubRel {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PubRel.class);
+
+    public void processPubRel(Channel channel, MqttMessageIdVariableHeader variableHeader) {
+        MqttMessage pubCompMessage = MqttMessageFactory.newMessage(
+                new MqttFixedHeader(MqttMessageType.PUBCOMP, false, MqttQoS.AT_MOST_ONCE, false, 0),
+                MqttMessageIdVariableHeader.from(variableHeader.messageId()),
+                null);
+        LOGGER.debug("PUBREL - clientId: {}, messageId: {}", (String) channel.attr(AttributeKey.valueOf("clientId")).get(), variableHeader.messageId());
+        channel.writeAndFlush(pubCompMessage);
+    }
 }
