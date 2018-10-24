18bfe48cf1995410763dfa88a4c937a6097e8093
sanshengshui <lovewsic@gmail.com>
2018-10-24 21:25:19 +0800

    :pencil2: 添加web认证用户名和密码接口

--- netty-iot/src/main/java/com/sanshengshui/iot/web/AuthApiController.java
+++ netty-iot/src/main/java/com/sanshengshui/iot/web/AuthApiController.java
@@ -1,17 +1,32 @@
 package com.sanshengshui.iot.web;
 
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.crypto.asymmetric.KeyType;
+import cn.hutool.crypto.asymmetric.RSA;
+import com.sanshengshui.iot.auth.util.PwdUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.interfaces.RSAPrivateKey;
+
+/**
+ * @author jamesmsw
+ * @date 2018年10月24号 21:24
+ */
 @RestController
 @RequestMapping("/groza/v1")
 @Slf4j
 public class AuthApiController {
 
-    @RequestMapping(value = "/test",method = RequestMethod.GET,produces = "application/json")
-    public String getAuthByUsername(){
-        return "Hello,World!";
+    @RequestMapping(value = "/{username}/auth",method = RequestMethod.GET,produces = "application/json")
+    public String getPwd(@PathVariable("username") String username){
+        RSAPrivateKey privateKey = IoUtil.readObj(PwdUtil.class.getClassLoader().getResourceAsStream("keystore/auth-private.key"));
+        RSA rsa = new RSA(privateKey, null);
+        return rsa.encryptBcd(username, KeyType.PrivateKey);
     }
+
+
 }
