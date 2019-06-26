1524cf994272000dac7bcbae8063d3c806b8a8b1
sanshengshui <lovewsic@gmail.com>
2019-06-26 14:29:00 +0800

    :beers: add contents for netty-jpa-mysql模块

---
+++ netty-jpa-mysql/src/main/java/com/sanshengshui/persistence/netty/NettServer.java
@@ -0,0 +1,84 @@
+package com.sanshengshui.persistence.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.ResourceLeakDetector;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+@ConfigurationProperties(prefix = "netty")
+@Slf4j
+public class NettServer {
+
+    @Value("${netty.bind_address}")
+    private String host;
+    @Value("${netty.bind_port}")
+    private Integer port;
+
+    @Value("${netty.boss_group_thread_count}")
+    private Integer bossGroupThreadCount;
+    @Value("${netty.worker_group_thread_count}")
+    private Integer workerGroupThreadCount;
+
+    private Channel serverChannel;
+    private EventLoopGroup bossGroup;
+    private EventLoopGroup workerGroup;
+
+    @PostConstruct
+    public void init() throws Exception {
+
+        log.info("Starting Netty Server");
+        bossGroup = new NioEventLoopGroup(bossGroupThreadCount);
+        workerGroup = new NioEventLoopGroup(workerGroupThreadCount);
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+                        pipeline.addLast("decoder", new StringDecoder());
+                        pipeline.addLast("encoder", new StringEncoder());
+                        NettyServerHandler nettyServerHandler = new NettyServerHandler();
+                        pipeline.addLast(nettyServerHandler);
+                    }
+                });
+
+        serverChannel = b.bind(host, port).sync().channel();
+
+
+        log.info("Netty Server started!");
+    }
+
+    @PreDestroy
+    public void shutdown() throws InterruptedException {
+        log.info("Stopping Netty Server!");
+        try {
+            serverChannel.close().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+        log.info("MQTT transport stopped!");
+    }
+}
---
+++ netty-jpa-mysql/src/main/java/com/sanshengshui/persistence/netty/NettyServerHandler.java
@@ -0,0 +1,54 @@
+package com.sanshengshui.persistence.netty;
+
+import io.netty.channel.*;
+
+import java.net.InetAddress;
+import java.util.Date;
+
+@ChannelHandler.Sharable
+public class NettyServerHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        // Send greeting for a new connection.
+        ctx.write("Welcome to " + InetAddress.getLocalHost().getHostName() + "!\r\n");
+        ctx.write("It is " + new Date() + " now.\r\n");
+        ctx.flush();
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String request) throws Exception {
+
+        String response;
+        boolean close = false;
+        if (request.isEmpty()) {
+            response = "Please type something.\r\n";
+        } else if ("bye".equals(request.toLowerCase())) {
+            response = "Have a good day!\r\n";
+            close = true;
+        } else {
+            response = "Did you say '" + request + "'?\r\n";
+        }
+
+        // We do not need to write a ChannelBuffer here.
+        // We know the encoder inserted at TelnetPipelineFactory will do the conversion.
+        ChannelFuture future = ctx.write(response);
+
+        // Close the connection after sending 'Have a good day!'
+        // if the client has sent 'bye'.
+        if (close) {
+            future.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
